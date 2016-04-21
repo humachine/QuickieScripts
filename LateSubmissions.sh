67359abd4719796aaaa975dd70214b961d1e6dbc
Humachine <swarun@gmail.com>
2016-04-21 00:33:06 -0700

    Cleaned up code. Added instructions for future usage

--- LateSubmissions.sh
+++ LateSubmissions.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+#This program is a script to use submission timestamps of my students and print all the late submissions
+
 #Read CSV and extract fields using \" as delimiter
 #Extract the names, student ID & date fields and clean up the text using sed
-#Use the paste command to combine them back into the same file
+#Eg: cat studentinfo.csv | awk '{print $1}' |sed 's/"//' >names
+# cat studentinfo.csv | awk '{print $4}' | sed 's/.*uploaded //' | sed 's/"//' >timesstamps
+
+#Use the paste command to combine them back into the same file (paste command creates a Tab-separated value list)
+#paste names timestamps >AllInfo
 
 deadline="2016-04-05 3:00pm"
 duedate=`date -d "$deadline" +%s`
 while read -r -a fullline
 do
-    line="${fullline[0]} ${fullline[1]}"
+    subtime="${fullline[0]} ${fullline[1]}"
     studentid=${fullline[2]}
     name=${fullline[@]:3}
-    d2=`date -d "$line" +%s`
+    d2=`date -d "$subtime" +%s`
 
     datediff=$(($d2-$duedate))
 
